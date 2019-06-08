6ea7cb05688a4ce9188342d36542abbbde765b1f
maceteli <47808850+maceteli@users.noreply.github.com>
2019-06-09 00:35:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openNav(){
+    //increases overlay to cover the screen
+    document.getElementById("cover").style.width = "100%";
+}
+function closeNav(){
+    //removes overlay from screenw
+    document.getElementById("cover").style.width = "0%";
+}
